eed9f042bf532dd07aa9cd49d7ec57737f142d74
GiovanniRodriguez <grodri41@illinois.edu>
2018-09-18 16:52:47 -0500

    Intial Commit for project, second one start

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,46 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
-    public static void main(String[] unused) {
-      System.out.println("Run");
+
+
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+
     }
+ public static void main( String[] unused) {
+     System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+ }
+
+public static void count( String[] unused) {
+    String texts = urlToString("http://erdani.com/tdpl/hamlet.txt");
+    int wordcount = 0;
+    for ( int i = 0; i <= texts.length(); i++) {
+        if (texts.charAt(i) == " "
+
+         }
+    }
+    System.out.println(words);
+  }
+
 }
+
+
+
+
